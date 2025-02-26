1633b726300d8a41711bde6fcb6e0ad2df915a8d
Sumit-Shastri <shastrisumit25@gmail.com>
2025-02-26 19:20:00 +0530

    step 3 - we succesfully fetched news from newsapi for eicher motors

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import pandas
 import yfinance as yf
+from newsapi import NewsApiClient
 
+#Eicher data fetching
 eicher = yf.Ticker("EICHERMOT.NS")
 eicher_data = eicher.history(period="5y")
 print(eicher_data.head())
+
+#News fetching
+newsapi = NewsApiClient(api_key="f1b9b05102bc4fee9fb107ce42f4cd14")
+articles = newsapi.get_everything(q="Eicher Motors", language="en", sort_by="publishedAt")
+
+# Display top 5 news headlines
+for article in articles["articles"][:5]:
+    print(article["title"], "-", article["url"])
+
+
+
 
 
